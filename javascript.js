2ccb67624cddf3fdb5b568943008ed9c652672c6
sellnat77 <sell_nat@yahoo.com>
2015-03-07 17:13:05 -0800

    Fixed some about stuff

--- javascript.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-$(window).scroll(function() {
-    if ($(window).scrollTop() > 400) {
-        $('.header').css("opacity", 1);
-    }
-    else{
-        $('.header').css("opacity", 0);
-    }
-});
